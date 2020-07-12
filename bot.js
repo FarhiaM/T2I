d2dab97c85a20c57756dcd30684cc09b01d60036
Farhia <farhia.fcb@gmail.com>
2020-07-12 19:48:21 +0200

    Impove image styling, add new font and change bot name handle

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var text2png = require('text2png');
 
 console.log("Bot up and running...");
 
-var stream = T.stream('statuses/filter', { track: '@InstaStoryTweet' });
+var stream = T.stream('statuses/filter', { track: '@tweet2image_bot' });
 stream.on('tweet', tweetEvent);
 
 function tweetEvent(tweet) {
@@ -17,19 +17,21 @@ function tweetEvent(tweet) {
     var txt = tweet.text;
     var mention = tweet.id_str;
 
-    if (txt.includes('@InstaStoryTweet')) {
+    if (txt.includes('@tweet2image_bot')) {
 
         //Get text of the tweet we are mentioned in
         T.get('statuses/show/:id', { id: tweet_mentioned_under }, function(err, data) {
 
             var imageText = data.text + '\n \n @' + tweet_author;
             fs.writeFileSync('out.png', text2png(imageText, {
-                font: '16px Futura',
+                font: '16px Roboto',
                 color: 'black',
-                backgroundColor: 'white',
-                lineSpacing: 10,
+                backgroundColor: 'linen',
+                lineSpacing: 7,
                 textAlign: 'center',
-                padding: 200
+                padding: 200,
+                localFontPath: 'Roboto-Regular.ttf',
+                localFontName: 'Roboto'
             }));
 
             var filePath = 'out.png';
@@ -56,7 +58,7 @@ function tweetEvent(tweet) {
                         }
 
                         T.post('statuses/update', params, function(err, data, response) {
-                            onsole.log("Posted reply with image");
+                            console.log("Posted reply with image");
                         })
                     }
                 })
